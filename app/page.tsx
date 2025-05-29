6515ab23bace17d61f0325e78c83079ae04fcd81
tmbekzod <tmcoderuz@gmail.com>
2025-05-29 17:52:46 +0500

    redirect to login if user is not authenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import { isLoggedIn } from "@/hooks/auth.ts";
+import { useEffect } from "react";
+
 export default function page() {
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      window.location.href = "/auth/login";
+    }
+  }, []);
   return <div>page</div>;
 }
