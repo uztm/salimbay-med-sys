3cd944e4a069c27d0ddd7938a39eddbb1ec0db23
tmbekzod <tmcoderuz@gmail.com>
2025-05-29 17:53:57 +0500

    redirect users to appropriate dashboard based on role after authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
 "use client";
 
 import { isLoggedIn } from "@/hooks/auth.ts";
+import { useUser } from "@/hooks/useUser";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function page() {
+  const { user, loading } = useUser();
+  const router = useRouter();
+
   useEffect(() => {
     if (!isLoggedIn()) {
       window.location.href = "/auth/login";
+    } else {
+      if (!loading && user) {
+        switch (user.role) {
+          case "AdminDoctor":
+            router.push("/dashboard/admin");
+            break;
+          case "Doctor":
+            router.push("/dashboard/doctor");
+            break;
+          case "Moderator":
+            router.push("/dashboard/nurse");
+            break;
+          case "Patient":
+            router.push("/dashboard/patient");
+            break;
+          default:
+            router.push("/");
+        }
+      }
     }
-  }, []);
+  }, [isLoggedIn, user, loading, router]);
   return <div>page</div>;
 }
